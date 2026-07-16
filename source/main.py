7843f35325deaf408214b8dfe83baa3c23aa3291
charleswiiflowers <carlitosfg.5051@gmail.com>
2026-07-15 21:56:50 -0600

    [🐮] Redoing the project

--- source/main.py
+++ source/main.py
@@ -1,17 +1,6 @@
-import os
-from mod.settings import load_settings, TEMPLATE
-
-CHARACTER_FOLDER:list[str] = [item for item in os.listdir("./assets/") if "." not in item]
-
-print(CHARACTER_FOLDER)
-
 class Main():
     def __init__(self) -> None:
-        self.settings = load_settings() if load_settings() != None else TEMPLATE
-
-    def validations(self) -> None:
-        if self.settings["selected_character"] in CHARACTER_FOLDER: # type: ignore
-            # :p
+        pass
 
 if __name__ == "__main__":
     Main()
--- source/mod/typething.py
+++
@@ -1,4 +0,0 @@
-from typing import TypedDict
-
-class Setting(TypedDict):
-    selected_character:str
