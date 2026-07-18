c4c84a7fbbfec063e2a18a3cfc69444cb51d9578
charleswiiflowers <carlitosfg.5051@gmail.com>
2026-07-17 23:34:08 -0600

    [🐯] Update Init

--- source/main.py
+++ source/main.py
@@ -1,13 +1,24 @@
 from core.events.event_bus import EventBus
 from core.config import Config
+from core.engine import Engine
+from systems.window_system import WindowSystem
+from actions.gravity import Gravity
 from utils.logger import Logger
 
 class Main():
     def __init__(self) -> None:
         self.bus = EventBus()
 
+        # Initialize core components
         self.config = Config(bus=self.bus)
         self.logger = Logger(bus=self.bus)
+        self.engine = Engine(bus=self.bus)
+
+        # Initialize systems
+        self.window_system = WindowSystem(bus=self.bus)
+
+        # Initialize actions
+        self.gravity = Gravity(bus=self.bus)
 
 if __name__ == "__main__":
     Main()
